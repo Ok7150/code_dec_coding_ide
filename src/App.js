cc6703444dcf5edce9d7f8d776974f776e6d23f9
Ok7150 <onkarvkale@official.com>
2022-11-10 12:34:25 +0530

    added styled components for left component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './screens/Home';
+import Playground from './screens/Playground';
+import Error404 from './screens/Error404';
+import { GlobalStyle } from './style/global';
+
 function App() {
   return (
-    <div>Hello</div>
+    <BrowserRouter>
+    <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/playground" element={<Playground />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
